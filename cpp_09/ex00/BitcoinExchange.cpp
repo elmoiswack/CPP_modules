d079b8f5e1fa4933d3a5a802710690d4be41696d
Dante Hussain <dhussain@f1r5s21.codam.nl>
2024-04-25 16:08:05 +0200

    finished

--- cpp_09/ex00/BitcoinExchange.cpp
+++ cpp_09/ex00/BitcoinExchange.cpp
@@ -141,6 +141,8 @@ void BitcoinExchange::CheckDate(std::string input)
 
 void BitcoinExchange::CheckValue(std::string input)
 {
+	if (input[0] == '-')
+		throw (BadValueTooLowException());
 	this->OverflowCheck(input);
 	double Value = std::stod(input);
 	if (Value < 0)
